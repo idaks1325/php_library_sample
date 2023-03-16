0f27998363ae73eb74474c3d07326a08f4ae4359
ryo.fujimoto <ryo.fujimoto@caters.co.jp>
2023-03-16 18:16:57 +0900

    fix: カウント合わない

--- tests/NamedSample1Test.php
+++ tests/NamedSample1Test.php
@@ -7,6 +7,7 @@ class NamedSample1Test extends TestCase {
     public function testSample() {
         $Sample = new NamedSample1();
         $Sample->add();
+        $Sample->add();
         $this->assertCount(2, $Sample->get());
     }
 }
