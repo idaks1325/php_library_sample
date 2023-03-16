b178c762cd8ec7ec705b34f819b1cb8b8e6211de
ryo.fujimoto <ryo.fujimoto@caters.co.jp>
2023-03-16 18:24:27 +0900

    modify: fw用

--- tests/NoNamedSample1Test.php
+++ tests/NoNamedSample1Test.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 class NoNamedSample1Test extends TestCase {
     public function testSample() {
-        $Sample = new NoNamedSample1();
+        $Sample = new \NoNamedSample1();
         $Sample->add();
         $Sample->add();
         $this->assertCount(2, $Sample->get());
